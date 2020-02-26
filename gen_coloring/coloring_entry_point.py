53702c421202c247fb7590a353c864adea9ac5e5
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-02-26 19:18:09 +0100

    Use decorators for language and generator registration

--- gen_coloring/coloring_entry_point.py
+++ gen_coloring/coloring_entry_point.py
@@ -1,29 +1,26 @@
-from textx import LanguageDesc, GeneratorDesc
+from textx import language, generator
 from .generators import load_metamodel
 from .generators.textmate_generator import TextMateGrammarGenerator
 from .utils import dump_to_file
 
 
-def easy_clr_metamodel():
+@language('EasyColorLang', '*.eclr')
+def easy_coloring_lang():
+    """
+    Language made for easier writing of TextMate grammars
+    """
     return load_metamodel()
 
 
-def fast_textmate_generator(metamodel, model, output_path, overwrite, debug, **custom_args):
+@generator('EasyColorLang', 'textmate')
+def textmate_gen_coloring(metamodel, model, output_path, overwrite, debug,
+                          **custom_args):
+    """
+    Language made for easier writing of TextMate grammars
+    """
     ret_val = TextMateGrammarGenerator(model).generate()
     if not output_path:
         print(ret_val)
     else:
         dump_to_file(ret_val, output_path)
     return ret_val
-
-
-easy_coloring_lang = LanguageDesc('EasyColorLang',
-                                  pattern='*.eclr',
-                                  description='Language made for easier writing of TextMate grammars',
-                                  metamodel=easy_clr_metamodel)
-
-textmate_gen_coloring = GeneratorDesc(
-    language='EasyColorLang',
-    target='textmate',
-    description='Language made for easier writing of TextMate grammars',
-    generator=fast_textmate_generator)
